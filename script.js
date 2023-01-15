000b163bb860e10aa30ce3f207fb918901167a86
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-16 00:44:41 +0100

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/*<script src="script.js"></script>*/
+
+let EmailInp = document.getElementById("email-inp");
+let EmailCont = document.querySelector(".email");
+let ErrorEl = document.querySelector(".error");
+let ErrorMsg = document.querySelector(".error-msg");
+let ValidMsg = document.querySelector(".valid-msg");
+let ButtonEl = document.getElementById("button");
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+ButtonEl.addEventListener("click", function () {
+    checkEmail()
+});
+
+function checkEmail() {
+    if (!EmailInp.value.match(mailformat)) {
+        EmailCont.style.border = "1px solid red";
+        ErrorEl.style.display = "block";
+        ErrorMsg.style.display = "block";
+    } else {
+        EmailCont.style.border = "1px solid var(--clr-primary-300)";
+        ErrorEl.style.display = "none";
+        ErrorMsg.style.display = "none";
+        ValidMsg.style.display = "block";
+    }
+}
